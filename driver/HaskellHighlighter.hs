0dff257732dc8740f4e745b3688998463304892f
Eric Mertens <emertens@gmail.com>
2015-03-14 15:06:30 -0700

    First draft of constraint polarity support

--- driver/HaskellHighlighter.hs
+++ driver/HaskellHighlighter.hs
@@ -1,11 +1,15 @@
 module HaskellHighlighter (highlightHaskell, highlightType) where
 
-import Data.List (intersperse)
+import Data.List (foldl',intersperse)
+import Data.Monoid
+import Data.Map (Map)
+import Data.Maybe (fromMaybe)
 import Language.Haskell.Lexer
 import Language.Haskell.Exts.Parser (parseWithMode, ParseMode(..), ParseResult(..))
 import Language.Haskell.Exts.Syntax (Asst(..), Context, TyVarBind(..), Type(..), Boxed(..),QName(Qual,UnQual), Name(..))
 import Language.Haskell.Exts.Extension (Extension(..), Language(Haskell2010), KnownExtension(..))
 import Language.Haskell.Exts.Pretty (Pretty, prettyPrintStyleMode, Style(mode), style, Mode(OneLineMode), defaultMode)
+import qualified Data.Map as Map
 
 highlightHaskell :: String -> String
 highlightHaskell src = init (colorize (lexerPass0 (src++"\n")))
@@ -80,6 +84,8 @@ pink   x = "\03\&13" ++ x ++ "\03\02\02"
 purple x = "\03\&06" ++ x ++ "\03\02\02"
 
 data Polarity = P | N | I | G | U
+  deriving (Show,Read, Eq)
+
 invert :: Polarity -> Polarity
 invert P = N
 invert N = P
@@ -91,77 +97,119 @@ highlightType :: String -> Maybe String
 highlightType = fmap renderType . myParseType
 
 renderType :: Type -> String
-renderType t = renderTypeS P t ""
+renderType t = renderTypeS P knownTypePolarities t ""
 
-renderTypeS :: Polarity -> Type -> ShowS
-renderTypeS polarity t0 =
+renderTypeS :: Polarity -> Map Name (Maybe [Polarity]) -> Type -> ShowS
+renderTypeS polarity facts t0 =
   case t0 of
     TyForall vars cxt t ->
          maybe id renderVars vars
        . renderCxt (invert polarity) cxt
-       . renderTypeS polarity t
+       . renderTypeS polarity facts' t
+      where
+      vars'  = fromMaybe [] vars
+      facts' = foldl' (flip learnFacts)
+                 (foldl' (flip Map.delete) facts (map tyVarName vars'))
+                 cxt
 
     TyFun f x ->
-         renderTypeS (invert polarity) f
+         renderTypeS (invert polarity) facts f
        . showString (orange " -> ")
-       . renderTypeS polarity x
+       . renderTypeS polarity facts x
 
     TyTuple boxed ts ->
          showTuple boxed
            (showCommaSep
-             (map (renderTypeS polarity) ts))
+             (map (renderTypeS polarity facts) ts))
 
     TyList t ->
          showChar '['
-       . renderTypeS polarity t
+       . renderTypeS polarity facts t
        . showChar ']'
 
     TyParArray t ->
          showString "[: "
-       . renderTypeS polarity t
+       . renderTypeS polarity facts t
        . showString " :]"
 
     -- TODO: much smarter
     TyCon n -> showString (polarityColor polarity (myPretty n))
     TyInfix l n r ->
-         renderTypeS U l
+         renderTypeS U facts l
        . showChar ' '
        . showString (polarityColor polarity (prettyOperator n))
        . showChar ' '
-       . renderTypeS U r
+       . renderTypeS U facts r
+
     TyApp f x ->
-         renderTypeS polarity f
-       . showChar ' '
-       . renderTypeS U x
+         renderTyApp polarity facts (TyApp f x)
 
     TyVar v -> showString (polarityColor polarity (myPretty v))
 
     TyKind t k ->
          showString "("
-       . renderTypeS polarity t
+       . renderTypeS polarity facts t
        . showString " :: "
        . showString (myPretty k)
        . showString ")"
 
     TyParen t ->
          showChar '('
-       . renderTypeS polarity t
+       . renderTypeS polarity facts t
        . showChar ')'
 
     TyEquals l r ->
-         renderTypeS U l
+         renderTypeS U facts l
        . showString (polarityColor polarity " ~ ")
-       . renderTypeS U r
+       . renderTypeS U facts r
 
     TyPromoted p -> showString (myPretty p) -- TODO
     TySplice s -> showString (myPretty s) -- TODO
-    TyBang _ t -> renderTypeS polarity t -- shouldn't occur...
+    TyBang _ t -> renderTypeS polarity facts t -- shouldn't occur...
+
+renderTyApp :: Polarity -> Map Name (Maybe [Polarity]) -> Type -> ShowS
+renderTyApp polarity facts t0 =
+  case appsToList t0 of
+    f:xs ->
+         renderTypeS polarity facts f
+       . foldr (.) id
+           (zipWith doOne info xs)
+      where
+      pickPolarity U = U
+      pickPolarity I = I
+      pickPolarity G = G
+      pickPolarity P = polarity
+      pickPolarity N = invert polarity
+
+      doOne p t = showChar ' '
+                . renderTypeS (pickPolarity p) facts t
+
+      info = case f of
+               TyVar n          -> info1 n
+               TyCon (UnQual n) -> info1 n
+               _                -> repeat U
+
+      info1 n = case Map.lookup n facts of
+                  Just (Just ps) -> ps
+                  _              -> repeat U
+
+    _ -> error "renderTyApp: bad type argument"
+
+
+-- This is making a bare-minimum effort
+appsToList :: Type -> [Type]
+appsToList (TyApp f x) = appsToList f ++ [x]
+appsToList x           = [x]
 
 renderVars :: [TyVarBind] -> ShowS
 renderVars vars
   = showString "forall"
   . showString (concatMap ((' ':). myPretty) vars)
   . showString ". "
+
+tyVarName :: TyVarBind -> Name
+tyVarName (KindedVar n _) = n
+tyVarName (UnkindedVar n) = n
 
 renderCxt :: Polarity -> Context -> ShowS
 renderCxt _ [] = id
@@ -176,23 +224,23 @@ renderCxt polarity xs
 showConstraint :: Polarity -> Asst -> ShowS
 showConstraint polarity (ClassA n ts)
   = showString (polarityColor polarity (myPretty n))
-  . showString (concatMap (\x -> ' ':renderTypeS U x "") ts)
+  . showString (concatMap (\x -> ' ':renderTypeS U mempty x "") ts)
 showConstraint polarity (VarA n)
   = showString (polarityColor polarity (myPretty n))
 showConstraint polarity (InfixA l n r)
-  = renderTypeS U l
+  = renderTypeS U mempty l
   . showString (polarityColor polarity (prettyOperator n))
-  . renderTypeS U r
+  . renderTypeS U mempty r
 showConstraint polarity (IParam n t)
   = showString "("
   . showString (myPretty n)
   . showString " :: "
-  . renderTypeS polarity t
+  . renderTypeS polarity mempty t
   . showString ")"
 showConstraint polarity (EqualP l r)
-  = renderTypeS U l
+  = renderTypeS U mempty l
   . showString (polarityColor polarity " ~ ")
-  . renderTypeS U r
+  . renderTypeS U mempty r
 
 showConstraint polarity (ParenA c)
   = showChar '('
@@ -213,7 +261,7 @@ showTuple Unboxed s = showString "(# " . s . showString " #)"
 showTuple Boxed   s = showString "(" . s . showString ")"
 
 showCommaSep :: [ShowS] -> ShowS
-showCommaSep = foldr (.) id . intersperse (showChar ',')
+showCommaSep = foldr (.) id . intersperse (showString ", ")
 
 polarityColor :: Polarity -> String -> String
 polarityColor P = green
@@ -243,5 +291,33 @@ myParseMode = ParseMode
 myPretty :: Pretty a => a -> String
 myPretty = prettyPrintStyleMode style { mode = OneLineMode } defaultMode
 
-knownTypePolarities :: [(Name, [Polarity])]
-knownTypePolarities = []
+learnFacts :: Asst -> Map Name (Maybe [Polarity]) -> Map Name (Maybe [Polarity])
+learnFacts (ClassA (UnQual klass) [TyVar t]) facts
+  | Just (Just xs) <- Map.lookup klass facts =
+      Map.insertWith mergeFactPolarity t (Just xs) facts
+learnFacts _ facts = facts -- TODO: invalidate stuff?
+
+mergeFactPolarity :: Maybe [Polarity] -> Maybe [Polarity] -> Maybe [Polarity]
+mergeFactPolarity mbNew mbOld =
+  do new <- mbNew
+     old <- mbOld
+     return (zipWith mergeFactPolarity1 new old)
+
+mergeFactPolarity1 :: Polarity -> Polarity -> Polarity
+mergeFactPolarity1 P N = I
+mergeFactPolarity1 N P = I
+mergeFactPolarity1 G x = x
+mergeFactPolarity1 x G = x
+mergeFactPolarity1 I _ = I
+mergeFactPolarity1 _ I = I
+mergeFactPolarity1 x y | x == y = x
+mergeFactPolarity1 _ _ = U
+
+knownTypePolarities :: Map Name (Maybe [Polarity])
+knownTypePolarities =
+  Map.fromList
+   [(Ident "Functor",Just [P])
+   ,(Ident "Contravariant",Just [N])
+   ,(Ident "Profunctor", Just [N,P])
+   ,(Ident "Bifunctor", Just [P,P])
+   ]
