a24c9623dff8b8b529937931c024114bf0f5bcc4
Eric Mertens <emertens@gmail.com>
2016-08-24 08:02:19 -0700

    Add type signature for view

--- lib/src/View.hs
+++ lib/src/View.hs
@@ -10,4 +10,6 @@ module View (view) where
 
 import Data.Functor.Const
 
+-- | Local definition of lens package's view.
+view :: ((a -> Const a a) -> s -> Const a s) -> s -> a
 view l x = getConst (l Const x)
