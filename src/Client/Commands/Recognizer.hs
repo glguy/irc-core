e6dcac8881cb24ecd92b4d2f6dcacef548794727
Dan Doel <dan.doel@gmail.com>
2016-09-05 00:32:38 -0400

    Recognizer documentation and cleanup

--- src/Client/Commands/Recognizer.hs
+++ src/Client/Commands/Recognizer.hs
@@ -25,13 +25,14 @@ import Control.Monad
 import Control.Applicative hiding (empty)
 
 import Data.HashMap.Strict (lookup,insertWith,HashMap,empty,unionWith,fromList,toList)
-import Data.List.NonEmpty (NonEmpty((:|)))
 import Data.Monoid
 import Data.Text (Text, commonPrefixes, cons, uncons, null)
 import Data.Maybe
 
 import Prelude hiding (all,null,lookup)
 
+-- | A map from 'Text' values to 'a' values that is capable of yielding more
+-- detailed information when looking up keys that are not actually in the map.
 data Recognizer a
   = Branch !Text !(Maybe a) !(HashMap Char (Recognizer a))
   deriving (Show, Functor)
@@ -40,13 +41,19 @@ instance Monoid (Recognizer a) where
   mempty = Branch "" Nothing empty
   mappend = both
 
+-- | Possible results of recognizing text.
 data Recognition a
-  = Exact a | Prefix [Text] | Invalid
+  = Exact a       -- ^ text matched exactly, yielding the given value
+  | Prefix [Text] -- ^ text would be recognized if joined to the given suffixes
+  | Invalid       -- ^ text could not possibly be recognized
   deriving (Show, Functor)
 
+-- | Match common prefixes of two strings in a more convenient form than
+-- available from 'Data.Text'
 common :: Text -> Text -> (Text, Text, Text)
 common l r = fromMaybe ("", l, r) $ commonPrefixes l r
 
+-- | Attempt to recognize a string, yielding a 'Recognition' result.
 recognize :: Text -> Recognizer a -> Recognition a
 recognize tx (Branch pf contained children)
   = case common pf tx of
@@ -61,9 +68,12 @@ recognize tx (Branch pf contained children)
           -> recognize txrest rec
         _ -> Invalid
 
+-- | Create a singleton 'Recognizer' associating the given 'Text' and value.
 single :: Text -> a -> Recognizer a
 single tx v = Branch tx (Just $! v) empty
 
+-- | Union two 'Recognizers'. The stored values in the result are biased to the
+-- left if there is key overlap.
 both :: Recognizer a -> Recognizer a -> Recognizer a
 both (Branch pfl conl chil) (Branch pfr conr chir)
   = case common pfl pfr of
@@ -82,24 +92,31 @@ both (Branch pfl conl chil) (Branch pfr conr chir)
                         , (r, Branch rest conr chir)
                         ]
 
+-- | Union an arbitrary number of 'Recognizers' as with 'both'.
 all :: [Recognizer a] -> Recognizer a
-all [] = Branch "" Nothing empty
+all [] = mempty
 all [r] = r
 all rs = all $ pair rs
  where
  pair (l:r:rest) = both l r : pair rest
  pair rest       = rest
 
+-- | Create a 'Recognizer' from an association list. If a key appears twice, the
+-- earliest associated value will be used.
 fromCommands :: [(Text, a)] -> Recognizer a
 fromCommands = all . map (uncurry single)
 
+-- | Add a key-value pair to a 'Recognizer'. This will override the value
+-- already present if one exists.
 addCommand :: Text -> a -> Recognizer a -> Recognizer a
 addCommand tx v = both $ single tx v
 
+-- | Compute all strings that will be recognized by a 'Recognizer'.
 keys :: Recognizer a -> [Text]
 keys (Branch pf contained children)
   = maybeToList (pf <$ contained)
   ++ (mappend pf <$> childKeys children)
 
+-- | Auxiliary function for 'keys'.
 childKeys :: HashMap Char (Recognizer a) -> [Text]
 childKeys children = toList children >>= \(c,rec) -> cons c <$> keys rec
