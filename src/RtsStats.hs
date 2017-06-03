bb4ccf2d498626ee6e22c5390c941f8cbb217b01
Eric Mertens <emertens@gmail.com>
2017-06-02 23:30:39 -0700

    Nicer text for /rtsstats

--- src/RtsStats.hs
+++ src/RtsStats.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE OverloadedStrings, FlexibleContexts, FlexibleInstances, DeriveGeneric, TypeOperators, StandaloneDeriving, CPP, EmptyCase #-}
-{-# OPTIONS_GHC -Wno-orphans -funfolding-use-threshold=2000 -funfolding-creation-threshold=1000 #-}
+{-# Language OverloadedStrings, CPP #-}
 
 {-|
 Module      : RtsStats
@@ -22,81 +21,25 @@ import           Data.List.Split (chunksOf)
 import           Data.Text (Text)
 import qualified Data.Text as Text
 import           Data.Word
-import           GHC.Generics
 import           GHC.Stats
 
-type KVs = [(Text,Text)] -> [(Text,Text)]
+class    Render a      where render :: a -> Text
+instance Render Word32 where render = prettyNum
+instance Render Word64 where render = prettyNum
+instance Render Int64  where render = prettyNum
+instance Render Double where render = Text.pack . show
 
-cons :: (Text, Text) -> KVs
-cons = (:)
-
-class    Fields a      where fields :: a -> Text -> KVs
-instance Fields Int64  where fields = commaFields
-instance Fields Word32 where fields = commaFields
-instance Fields Word64 where fields = commaFields
-instance Fields Double where fields = showFields
-
-showFields :: Show a => a -> Text -> KVs
-showFields x n = cons (n, Text.pack (show x))
-{-# NOINLINE showFields #-}
-
-commaFields :: Show a => a -> Text -> KVs
-commaFields x n = cons (n, Text.pack (addCommas (show x)))
-{-# NOINLINE commaFields #-}
+prettyNum :: Show a => Num a => Ord a => a -> Text
+prettyNum n = Text.pack (addSign (addCommas (show (abs n))))
+  where
+    addSign = if n < 0 then (:) '-' else id
 
 addCommas :: String -> String
 addCommas = reverse . intercalate "," . chunksOf 3 . reverse
 
-genericFields :: (Generic a, GFields (Rep a)) => a -> Text -> KVs
-genericFields = gfields . from
-
-class GFields f where
-  gfields :: f p -> Text -> KVs
-
-instance GFields f => GFields (D1 c f) where
-  gfields (M1 x) = gfields x
-  {-# INLINE gfields #-}
-
-instance GFields f => GFields (C1 c f) where
-  gfields (M1 x) = gfields x
-  {-# INLINE gfields #-}
-
-instance (Selector s, GFields f) => GFields (S1 s f) where
-  gfields s@(M1 x) _ = gfields x (Text.pack (selName s))
-  {-# INLINE gfields #-}
-
-instance (GFields f, GFields g) => GFields (f :*: g) where
-  gfields (x :*: y) n = gfields x n . gfields y n
-  {-# INLINE gfields #-}
-
-instance (GFields f, GFields g) => GFields (f :+: g) where
-  gfields (L1 x) = gfields x
-  gfields (R1 x) = gfields x
-  {-# INLINE gfields #-}
-
-instance GFields U1 where
-  gfields _ _ = id
-  {-# INLINE gfields #-}
-
-instance GFields V1 where
-  gfields v _ = case v of {}
-  {-# INLINE gfields #-}
-
-instance Fields a => GFields (K1 i a) where
-  gfields (K1 x) = fields x
-  {-# INLINE gfields #-}
-
-statsToEntries :: Stats -> [(Text, Text)]
-statsToEntries (Stats rts) = fields rts "stats" []
-
 #if MIN_VERSION_base(4,10,0)
 
 newtype Stats = Stats RTSStats
-
-deriving instance Generic RTSStats
-deriving instance Generic GCDetails
-instance Fields RTSStats  where fields = genericFields
-instance Fields GCDetails where fields = genericFields
 
 getStats :: IO (Maybe Stats)
 getStats =
@@ -104,12 +47,74 @@ getStats =
      if enabled then Just . Stats <$> getRTSStats
                 else pure Nothing
 
+statsToEntries :: Stats -> [(Text, Text)]
+statsToEntries (Stats rts) = seq rgc
+  [ ("GCs"                                 , render $ gcs                             rts)
+  , ("Major GCs"                           , render $ major_gcs                       rts)
+  , ("Allocated bytes"                     , render $ allocated_bytes                 rts)
+  , ("Max live bytes"                      , render $ max_live_bytes                  rts)
+  , ("Max large objects bytes"             , render $ max_large_objects_bytes         rts)
+  , ("Max compact bytes"                   , render $ max_compact_bytes               rts)
+  , ("Max slop bytes"                      , render $ max_slop_bytes                  rts)
+  , ("Max memory in use bytes"             , render $ max_mem_in_use_bytes            rts)
+  , ("Cumulative live bytes"               , render $ cumulative_live_bytes           rts)
+  , ("Copied bytes"                        , render $ copied_bytes                    rts)
+  , ("Parallel copied bytes"               , render $ par_copied_bytes                rts)
+  , ("Cumulative parallel max copied bytes", render $ cumulative_par_max_copied_bytes rts)
+  , ("Mutator CPU ns"                      , render $ mutator_cpu_ns                  rts)
+  , ("Mutator elapsed ns"                  , render $ mutator_elapsed_ns              rts)
+  , ("GC CPU ns"                           , render $ gc_cpu_ns                       rts)
+  , ("GC elapsed ns"                       , render $ gc_elapsed_ns                   rts)
+  , ("CPU ns"                              , render $ cpu_ns                          rts)
+  , ("Elapsed ns"                          , render $ elapsed_ns                      rts)
+  , ("==[Totals]=="                        , ""                                          )
+  , (""                                    , ""                                          )
+  , ("Generation"                          , render $ gcdetails_gen                   rgc)
+  , ("Threads"                             , render $ gcdetails_threads               rgc)
+  , ("Allocated bytes"                     , render $ gcdetails_allocated_bytes       rgc)
+  , ("Live bytes"                          , render $ gcdetails_live_bytes            rgc)
+  , ("Large objects bytes"                 , render $ gcdetails_large_objects_bytes   rgc)
+  , ("Compact bytes"                       , render $ gcdetails_compact_bytes         rgc)
+  , ("Slop bytes"                          , render $ gcdetails_slop_bytes            rgc)
+  , ("Memory in use bytes"                 , render $ gcdetails_mem_in_use_bytes      rgc)
+  , ("Copied bytes"                        , render $ gcdetails_copied_bytes          rgc)
+  , ("Parallel max copied bytes"           , render $ gcdetails_par_max_copied_bytes  rgc)
+  , ("Sync elapsed ns"                     , render $ gcdetails_sync_elapsed_ns       rgc)
+  , ("CPU ns"                              , render $ gcdetails_cpu_ns                rgc)
+  , ("Elapsed ns"                          , render $ gcdetails_elapsed_ns            rgc)
+  , ("==[Last GC]=="                       , ""                                          )
+  ]
+  where
+    rgc = gc rts
+
 #else
 
-newtype Stats = Stats GCStats
+statsToEntries :: Stats -> [(Text, Text)]
+statsToEntries (Stats rts) = seq rts
+  [ ("Allocated bytes"            , render $ bytesAllocated         rts)
+  , ("GCs"                        , render $ numGcs                 rts)
+  , ("Major GCs"                  , render $ numByteUsageSamples    rts)
+  , ("Cumulative allocated bytes" , render $ maxBytesUsed           rts)
+  , ("Cumulative live bytes"      , render $ cumulativeBytesUsed    rts)
+  , ("Cumulative copied  bytes"   , render $ bytesCopied            rts)
+  , ("Max slop bytes"             , render $ maxBytesSlop           rts)
+  , ("Maximum allocated megabytes", render $ peakMegabytesAllocated rts)
+  , ("Mutator CPU seconds"        , render $ mutatorCpuSeconds      rts)
+  , ("Mutator elapsed seconds"    , render $ mutatorWallSeconds     rts)
+  , ("GC CPU seconds"             , render $ gcCpuSeconds           rts)
+  , ("GC elapsed seconds"         , render $ gcWallSeconds          rts)
+  , ("CPU seconds"                , render $ cpuSeconds             rts)
+  , ("Elapsed seconds"            , render $ wallSeconds            rts)
+  , ("Total bytes copied"         , render $ parTotBytesCopied      rts)
+  , ("Max parallel bytes copied"  , render $ parMaxBytesCopied      rts)
+  , ("==[Totals]=="               , ""                                 )
+  , (""                           , ""                                 )
+  , ("(last) Live bytes"          , render $ currentBytesUsed       rts)
+  , ("(last) Slop bytes"          , render $ currentBytesSlop       rts)
+  , ("==[Last GC]=="              , ""                                 )
+  ]
 
-deriving instance Generic GCStats
-instance Fields GCStats where fields = genericFields
+newtype Stats = Stats GCStats
 
 getStats :: IO (Maybe Stats)
 getStats =
