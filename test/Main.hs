f8a3c86f2ee9198002b05dd0c7962db813e7b2a5
Eric Mertens <emertens@galois.com>
2017-08-29 19:33:49 -0700

    Fix parser tests

--- test/Main.hs
+++ test/Main.hs
@@ -2,7 +2,7 @@
 {-|
 Module      : Main
 Description : Tests for the glirc library
-Copyright   : (c) Eric Mertens, 2016
+Copyright   : (c) Eric Mertens, 2017
 License     : ISC
 Maintainer  : emertens@gmail.com
 
@@ -11,7 +11,9 @@ This module test various components of glirc
 -}
 module Main (main) where
 
-import           Client.Commands.Arguments
+import           Client.Commands.Arguments.Spec
+import           Client.Commands.Arguments.Parser
+import           Control.Applicative
 import           System.Exit
 import           Test.HUnit
 
@@ -29,27 +31,43 @@ argumentParserTests :: Test
 argumentParserTests = test
   [ assertEqual "no arg empty"
        (Just ())
-       (parseArguments NoArg "")
+       (parse () (pure ()) "")
   , assertEqual "no arg white"
        (Just ())
-       (parseArguments NoArg "   ")
+       (parse () (pure ()) "   ")
   , assertEqual "no arg fail"
        Nothing
-       (parseArguments NoArg " a")
+       (parse () (pure ()) " a")
 
   , assertEqual "required"
-       (Just ("argument",()))
-       (parseArguments (ReqTokenArg "field" NoArg) " argument ")
+       (Just "argument")
+       (parse () (simpleToken "field") " argument ")
 
   , assertEqual "optional 1"
-       (Just (Just ("argument",())))
-       (parseArguments (OptTokenArg "field" NoArg) " argument ")
+       (Just (Just "argument"))
+       (parse () (optionalArg (simpleToken "field")) " argument ")
 
   , assertEqual "optional 2"
        (Just Nothing)
-       (parseArguments (OptTokenArg "field" NoArg) "  ")
+       (parse () (optionalArg (simpleToken "field")) "  ")
 
   , assertEqual "remaining"
        (Just "some stuff ")
-       (parseArguments (RemainingArg "field") " some stuff ")
+       (parse () (remainingArg "field") " some stuff ")
+
+  , assertEqual "token and empty remaining"
+       (Just ("some", ""))
+       (parse () (liftA2 (,) (simpleToken "first") (remainingArg "second")) " some")
+
+  , assertEqual "token and empty remaining 1"
+       (Just ("some", ""))
+       (parse () (liftA2 (,) (simpleToken "first") (remainingArg "second")) "  some ")
+
+  , assertEqual "token and remaining"
+       (Just ("some", "text here"))
+       (parse () (liftA2 (,) (simpleToken "first") (remainingArg "second")) " some text here")
+
+  , assertEqual "extra whitespace token and remaining"
+       (Just ("some", " text here"))
+       (parse () (liftA2 (,) (simpleToken "first") (remainingArg "second")) "  some  text here")
   ]
