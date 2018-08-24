24ca5b7e1dae7052855583a38eed5f2bc483e5ed
Eric Mertens <emertens@galois.com>
2018-08-24 09:30:56 -0700

    Better error detection in glirc.set_focus

--- lua-extension/glirc-lib.c
+++ lua-extension/glirc-lib.c
@@ -356,6 +356,9 @@ static int glirc_lua_set_focus(lua_State *L)
         size_t network_len = 0, target_len = 0;
         const char *network = luaL_checklstring(L, 1, &network_len);
         const char *target  = luaL_checklstring(L, 2, &target_len);
+        luaL_argcheck(L, network_len > 0 || target_len == 0, 1,
+                      "target specified without a network");
+        luaL_checktype(L, 3, LUA_TNONE);
         glirc_set_focus(get_glirc(L), network, network_len, target,  target_len);
         return 0;
 }
