f1c6a2a0c8d5e4291db2018bc81c479c1e41b58f
Eric Mertens <emertens@gmail.com>
2020-08-20 09:13:11 -0700

    simplify concurrent connect

--- hookup/src/Hookup/Concurrent.hs
+++ hookup/src/Hookup/Concurrent.hs
@@ -9,68 +9,76 @@ Maintainer  : emertens@gmail.com
 -}
 module Hookup.Concurrent (concurrentAttempts) where
 
-import Control.Concurrent (forkIO)
-import Control.Concurrent.Async (Async, async, cancel, waitCatchSTM)
+import Control.Concurrent (forkIO, throwTo)
+import Control.Concurrent.Async (Async, AsyncCancelled(..), async, asyncThreadId, cancel, waitCatchSTM)
 import Control.Concurrent.STM (STM, atomically, check, orElse, readTVar, registerDelay, retry)
 import Control.Exception (SomeException, finally, mask_, onException)
-import Data.Foldable (traverse_)
+import Control.Monad (join)
+import Data.Foldable (for_)
 
 concurrentAttempts ::
   Int {- ^ microsecond delay between attempts -} ->
   [IO a] {- ^ ordered list of attempts -} ->
   IO (Either [SomeException] a)
-concurrentAttempts delay actions = mask_ (start (initialSt delay actions))
+concurrentAttempts _ [] = pure (Left [])
+concurrentAttempts delay actions =
+  let st = St { threads = [],
+                errors = [],
+                work = actions,
+                delay = delay,
+                readySTM = retry }
+  in mask_ (stall st)
 
 data St a = St
   { threads :: [Async a]
   , errors  :: [SomeException]
   , work    :: [IO a]
   , delay   :: !Int
+  , readySTM :: STM ()
   }
 
-initialSt :: Int -> [IO a] -> St a
-initialSt delay xs = St
-  { threads = []
-  , errors  = []
-  , work    = xs
-  , delay   = delay
-  }
-
+-- non-blocking cancelation of the remaining threads
 cleanup :: [Async a] -> IO ()
-cleanup xs = () <$ forkIO (traverse_ cancel xs)
+cleanup xs = () <$ forkIO (for_ xs \x -> throwTo (asyncThreadId x) AsyncCancelled)
 
 type Answer a = IO (Either [SomeException] a)
 
-start :: St a -> Answer a
-start st =
-  case work st of
-    [] -> stall st
-    x:xs -> do thread <- async x
-               stall st { threads = thread : threads st, work = xs }
+start :: IO a -> St a -> Answer a
+start x st =
+  do thread <- async x
+     ready <- if null (work st) then pure retry else startTimer (delay st)
+     stall st { threads = thread : threads st, readySTM = ready }
 
 stall :: forall a. St a -> Answer a
-stall st
-  | null (threads st), null (work st) = pure (Left (errors st))
-  | otherwise =
-  do fresh <- mkFresh
-     next <- atomically (fresh `orElse` finish [] (threads st))
-             `onException` cleanup (threads st)
-     next
+stall st = if null (threads st) then nothingRunning else waitForEvent
   where
-    mkFresh :: IO (STM (Answer a))
-    mkFresh
-      | null (work st) = pure retry
-      | otherwise  = do tv <- registerDelay (delay st)
-                        pure do check =<< readTVar tv
-                                pure (start st)
+    nothingRunning =
+      case work st of
+        [] -> pure (Left (errors st))
+        x:xs -> start x st{ work = xs }
+
+    waitForEvent = join (atomically (finish [] (threads st))
+                         `onException` cleanup (threads st))
+
+    fresh :: STM (Answer a)
+    fresh =
+      case work st of
+        [] -> retry
+        x:xs -> start x st{work = xs} <$ readySTM st
 
     finish :: [Async a] -> [Async a] -> STM (Answer a)
-    finish threads' [] = retry
-    finish threads' (t:ts) = finish1 (threads' ++ ts) t `orElse` finish (t:threads') ts
+    finish threads' [] = fresh
+    finish threads' (t:ts) = finish1 (threads' ++ ts) t
+                    `orElse` finish (t:threads') ts
 
     finish1 :: [Async a] -> Async a -> STM (Answer a)
     finish1 threads' t =
       do res <- waitCatchSTM t
          pure case res of
            Right s -> Right s <$ cleanup threads'
-           Left e -> start st { errors = e : errors st, threads = threads'}
+           Left e -> stall st { errors = e : errors st, threads = threads'}
+
+startTimer :: Int -> IO (STM ())
+startTimer n =
+  do v <- registerDelay n
+     pure (check =<< readTVar v)
